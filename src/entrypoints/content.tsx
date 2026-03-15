0fa7770a0e0958c050fb360bc9c97fef800decaa
ErickLimonG <tolsericko@hotmai.com>
2026-03-15 12:24:30 -0600

    Removed unused imports from content.tsx

--- src/entrypoints/content.tsx
+++ src/entrypoints/content.tsx
@@ -1,13 +1,4 @@
-import addonMatcher from "@/Addons/AddonMatcher";
-import availableAddons from "@/Addons/AvailableAddons";
-import logger from "@/logger";
-import { createPortal } from "react-dom";
-import ReactDOM from "react-dom/client";
-import Widget from "@/interfaces/Widget";
-import { WidgetConstructor } from "@/interfaces/WidgetConstructor";
-import { createContext } from "react";
 import AddonManager from "@/Addons/AddonManager";
-//import ToggleAllWeekButton from "@/Addons/ParentalControl/ToggleAllWeek/ToggleAllWeekButton";
 
 export default defineContentScript({
   matches: ['*://*/*'],
