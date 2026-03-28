fc9e952261a09dcbec842dd961d60aee307a1e0c
ErickLimonG <tolsericko@hotmai.com>
2026-03-27 18:20:24 -0600

    Modified widget constructor and interface

--- src/interfaces/Widget.ts
+++ src/interfaces/Widget.ts
@@ -2,5 +2,5 @@ import React from "react";
 
 export default interface Widget {
   destinationQuery: string;
-  init(iframeDoc: Document): React.JSX.Element;
+  component: React.JSX.Element;
 }
--- src/interfaces/WidgetConstructor.ts
+++ src/interfaces/WidgetConstructor.ts
@@ -1,5 +1,5 @@
 import Widget from "./Widget";
 
-type WidgetConstructor = new () => Widget;
+type WidgetConstructor = new (document: Document) => Widget;
 
 export { WidgetConstructor };
