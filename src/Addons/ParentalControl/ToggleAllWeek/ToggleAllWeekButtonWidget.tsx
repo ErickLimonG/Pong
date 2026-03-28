6173816c67f51dd10e45ba7728298fcb7d4b8a3b
ErickLimonG <tolsericko@hotmai.com>
2026-03-27 18:24:43 -0600

    Made DomAdapter for ToggleAllWeekWidget

--- src/Addons/ParentalControl/ToggleAllWeek/ToggleAllWeekButtonWidget.tsx
+++ src/Addons/ParentalControl/ToggleAllWeek/ToggleAllWeekButtonWidget.tsx
@@ -1,24 +1,20 @@
 import Widget from "@/interfaces/Widget";
 import ToggleAllWeekButton from "./ToggleAllWeekButton";
-import logger from "@/logger";
-
-const PARENT_CONTROL_FORM = "form[name=formParentCtrlAdd]";
-const daysTableString =
-  PARENT_CONTROL_FORM +
-  " " +
-  "div:first-child > table:nth-of-type(2) tr:nth-of-type(2)";
+import { ToggleAllWeekDomAdapter } from "./DomAdapter/ToggleAllWeekDomAdapter";
+import IToggleAllWeekDomAdapter from "./DomAdapter/IToggleAllWeekDomAdapter";
 
 class ToggleAllWeekWidget implements Widget {
   readonly destinationQuery =
-    PARENT_CONTROL_FORM +
-    " " +
-    "div:first-child > table:nth-of-type(2) tr:nth-of-type(2)" +
-    " th:first-child";
+    "body > form:nth-child(3) > div:nth-child(1) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(2) > th:nth-child(1)";
+  private adapter: IToggleAllWeekDomAdapter;
 
-  init(iframeDoc: Document) {
-    logger.debug("ToggleAllWeekWidget instantiated");
-    const inputs = iframeDoc.querySelectorAll(daysTableString + " > td input");
-    const allDayInputs = Array.from(inputs) as HTMLInputElement[];
+  constructor(document: Document) {
+    this.adapter = new ToggleAllWeekDomAdapter(document);
+  }
+
+  get component() {
+    const dayInputs = this.adapter.findDayInputs();
+    const allDayInputs = dayInputs as HTMLInputElement[];
     return <ToggleAllWeekButton allDayInputs={allDayInputs} />;
   }
 }
