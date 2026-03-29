93275dd1e5654851f7235817b0736b28751d2723
ErickLimonG <tolsericko@hotmai.com>
2026-03-29 08:36:38 -0600

    Refactored ToggleAllWeekWidget to be compliant with new widget interface

--- src/Addons/ParentalControl/ToggleAllWeek/ToggleAllWeekButtonWidget.tsx
+++ src/Addons/ParentalControl/ToggleAllWeek/ToggleAllWeekButtonWidget.tsx
@@ -2,20 +2,26 @@ import Widget from "@/interfaces/Widget";
 import ToggleAllWeekButton from "./ToggleAllWeekButton";
 import { ToggleAllWeekDomAdapter } from "./DomAdapter/ToggleAllWeekDomAdapter";
 import IToggleAllWeekDomAdapter from "./DomAdapter/IToggleAllWeekDomAdapter";
+import component from "@/interfaces/component";
 
 class ToggleAllWeekWidget implements Widget {
-  readonly destinationQuery =
-    "body > form:nth-child(3) > div:nth-child(1) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(2) > th:nth-child(1)";
   private adapter: IToggleAllWeekDomAdapter;
 
   constructor(document: Document) {
     this.adapter = new ToggleAllWeekDomAdapter(document);
   }
 
-  get component() {
+  get components() {
     const dayInputs = this.adapter.findDayInputs();
     const allDayInputs = dayInputs as HTMLInputElement[];
-    return <ToggleAllWeekButton allDayInputs={allDayInputs} />;
+
+    const toggleAllWeek: component = {
+      destinationQuery:
+        "body > form:nth-child(3) > div:nth-child(1) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(2) > th:nth-child(1)",
+      component: <ToggleAllWeekButton allDayInputs={allDayInputs} />,
+    };
+
+    return [toggleAllWeek];
   }
 }
 
