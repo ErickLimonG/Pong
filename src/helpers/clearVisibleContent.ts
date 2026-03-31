1264b0489013e5fa0473defcef83d1226ec28c6a
ErickLimonG <tolsericko@hotmai.com>
2026-03-31 14:40:21 -0600

    Rename cleanElementChildren function to clearVisibleContent

--- src/helpers/clearVisibleContent.ts
+++ src/helpers/clearVisibleContent.ts
@@ -2,11 +2,11 @@ function cleanNodes(node: Node) {
   // Hide the original inputs
   if (node.nodeType === Node.ELEMENT_NODE)
     (node as Element).setAttribute("hidden", "true");
-  // Remove the ":" text nodes
+  // Remove the text nodes
   if (node.nodeType === Node.TEXT_NODE) node.parentNode?.removeChild(node);
 }
 
-export default function cleanElementChildren(element: HTMLElement) {
+export default function clearVisibleContent(element: HTMLElement) {
   if (!element) return;
   const children = [...element.childNodes];
   children.forEach(cleanNodes);
